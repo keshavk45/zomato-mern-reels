4094f721f992e8ec29e61aac988a972dd397d8e1
keshavk45 <keshavk0682@gmail.com>
2026-02-06 00:27:35 +0530

    Refactor registerUser function to include user creation and JWT token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -15,4 +15,30 @@ async function registerUser(req,res){
             message : "User already exists"
         })
     }
+
+
+    const hashedPassword = await bcrypt.hash(password , 10);
+
+    const user = await userModel.create({
+        fullName , email,password : hashedPassword
+    })
+// ab user agar register toh ho gaya lekin agar jab woh baar baar aya toh pata chal sake ki woh kaha se register kar raha h toh uski details ko store karte h tokens m and unn tokens ko store karte h cookies m
+//tokens kliye jasonwebpatocken aur cookies m token ko save k  k liye cookiesparcel
+    const token = jwt.sign({
+        id : user._id,
+    } , process.env.JWT_SECRET)
+
+    //TO ACCESSS JWT TOKENS WE USE PACKAGE DOTENV
+    res.cookie("token" , token )
+    res.status(201).json({
+        message : "User registered successfully",
+        user:{
+            _id : user._id,
+            fullName : user.fullName,
+            email : user.email,
+        }
+    })
+
+
+
 }
