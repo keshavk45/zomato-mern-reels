873e1ac008b78458568c05d1eb2423a04739ddea
keshavk45 <keshavk0682@gmail.com>
2026-02-08 23:40:33 +0530

    Add user authentication routes for login and logout

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,3 +6,6 @@ const authController = require('../controllers/auth.controller');
 //user auh APIs
 //api ko use karte waqt ham callback ko controler bhi kehte h
 router.post('/user/register', authController.registerUser)
+router.post('/user/login', authController.loginUser)
+router.get('/user/logout', authController.logoutUser)
+module.exports = router;
